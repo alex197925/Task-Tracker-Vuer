321758838ea75169cd83cf8dcb06ec34e667182a
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-10-31 16:25:12 +0100

    Added Components Tasks and Task with CSS

---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<!-- @format -->
+
+<!-- What I want to do here is, take prop of tasks, take an array and loop them and output single task component for each one. -->
+
+<template>
+  <!-- Looping through tasks and displaying them -->
+  <div v-for="task in tasks" :key="task.id">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+};
+</script>
