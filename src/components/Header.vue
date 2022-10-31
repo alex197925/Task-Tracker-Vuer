208610882798fdaceb8729e2c12154c12e0f2631
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-10-31 13:05:05 +0100

    Added first component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<!-- @format -->
+
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Scoped - style only for this componenet -->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
